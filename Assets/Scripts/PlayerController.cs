829bfb77e73af6fab1de5e587fcf1f4712c85734
yslnapa <ysl.napa@gmail.com>
2017-07-21 20:36:15 +0900

    Original

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public float speed;
+    private Rigidbody rb;
+
+    Void Start ()
+    {
+        rb = GetComponent<Rigidbody>();        
+    }
+
+    private void FixedUpdate()
+    {
+        float MoveHorizontal = Input.GetAxis("Horizontal");
+        float MoveVertical = Input.GetAxis("Vertical");
+
+        Vector3 moverment = new Vector3(MoveHorizontal, 0.0f, MoveVertical);
+
+        rb.AddForce(moverment * speed)
+    }
+
+
+
+}
